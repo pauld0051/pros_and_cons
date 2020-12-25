6dc817f603291ba586d8027d496c259b32976449
pauld0051 <62159791+pauld0051@users.noreply.github.com>
2020-12-25 17:01:08 +0100

    Fix dropdown for both mobile and desktop

--- static/scripts/dependencies.js
+++ static/scripts/dependencies.js
@@ -1,5 +1,5 @@
 // Site wide JS
 $(document).ready(function() {
     $(".sidenav").sidenav({ edge: "right" });
-    $(".dropdown-trigger").dropdown({ hover: false });
+    $(".dropdown-trigger").dropdown({ hover: false, 'coverTrigger': false, 'closeOnClick': true });
 });

01a9f1a78cb9f25ee41cef62e7c7f4a0e1a0af1b
pauld0051 <62159791+pauld0051@users.noreply.github.com>
2020-12-25 11:49:26 +0100

    Restart app to join to Heroku

--- app.py
+++ app.py
@@ -1,31 +1,18 @@
 import os
-from flask import (
-    Flask, flash, render_template,
-    redirect, request, session, url_for)
-from flask_pymongo import PyMongo
-from bson.objectid import ObjectId
-from werkzeug.security import generate_password_hash, check_password_hash
+from flask import Flask
 if os.path.exists("env.py"):
     import env
 
 
-app = Flask(__name__)
-
-app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
-app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
-app.secret_key = os.environ.get("SECRET_KEY")
-
-mongo = PyMongo(app)
+app = Flask (__name__)
 
 
 @app.route("/")
+def hello():
+    return "Hello, this is Paul testing stuff"
 
-@app.route("/register", methods=["GET", "POST"])
-def register():
-    return render_template("register.html")
-        
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
-            port=int(os.environ.get("PORT")),
-            debug=True)
+    port=int(os.environ.get("PORT")),
+    debug=True)

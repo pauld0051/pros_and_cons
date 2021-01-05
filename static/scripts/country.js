738e0084f745d1494c2025227eae61971a195459
pauld0051 <62159791+pauld0051@users.noreply.github.com>
2021-01-05 21:00:22 +0100

    Fix country dropdown list width allowing country selection to be read in JS

--- static/scripts/country.js
+++ static/scripts/country.js
@@ -1,11 +1,6 @@
 // Get the chosen country after change. First country is now "not specified" 
 // which means if any country is chosen, there should be a change. 
-function countries() {
-    document.getElementById("country").addEventListener("change", function() {
+document.getElementById("country").addEventListener("change", function() {
     let country_chosen = document.getElementById("country").value;
     console.log(country_chosen);
   });
-}
-
-// Let the page load before the script is called.
-let timeOut = setTimeout(countries, 2000);

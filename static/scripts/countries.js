cf835e49a1ce33350ad5c891e3f9701fc06dde1e
pauld0051 <62159791+pauld0051@users.noreply.github.com>
2021-01-09 12:01:56 +0100

    Allow users to chose state only if country is already pre-selected

--- static/scripts/countries.js
+++ static/scripts/countries.js
@@ -10,9 +10,6 @@ $('#state').on('contentChanged', function () {
 const selectTag = document.getElementById("state");
 document.getElementById("country").addEventListener("change", function () {
   const country_change = document.getElementById("country").value;
-  console.log(country_change);
-  console.log(countries);
-  console.log(selectTag);
   const statesArray = [];
   const Ind = countries.findIndex(e => {
       return e['name'] === country_change;
@@ -22,7 +19,58 @@ document.getElementById("country").addEventListener("change", function () {
   }
   let str = "";
   statesArray.forEach(e => {
-      console.log(e);
+      str += `<option value="${e}">${e}</option>`;
+  })
+  selectTag.innerHTML = str;
+  $("#state").trigger('contentChanged');
+});
+
+document.getElementById("country").addEventListener("load", function () {
+  const state_change = document.getElementById("country").value;
+  const states_changeArray = [];
+  const Ind_state = countries.findIndex(e => {
+      return e['name'] === state_change;
+  })
+  if (Ind_state != -1) {
+      countries[Ind_state]['states'].forEach(e => states_changeArray.push(e['name']));
+  }
+  let str = "";
+  states_changeArray.forEach(e => {
+      str += `<option value="${e}">${e}</option>`;
+  })
+  selectTag.innerHTML = str;
+  $("#state").trigger('contentChanged');
+});
+
+$(document).ready(function() { 
+  const country_change = document.getElementById("country").value;
+  console.log(country_change)
+  const statesArray = [];
+  const Ind = countries.findIndex(e => {
+      return e['name'] === country_change;
+  })
+  if (Ind != -1) {
+      countries[Ind]['states'].forEach(e => statesArray.push(e['name']));
+  }
+  let str = "";
+  statesArray.forEach(e => {
+      str += `<option value="${e}">${e}</option>`;
+  })
+  selectTag.innerHTML = str;
+  $("#state").trigger('contentChanged');
+});
+
+document.getElementById("country").addEventListener("load", function () {
+  const state_change = document.getElementById("country").value;
+  const states_changeArray = [];
+  const Ind_state = countries.findIndex(e => {
+      return e['name'] === state_change;
+  })
+  if (Ind_state != -1) {
+      countries[Ind_state]['states'].forEach(e => states_changeArray.push(e['name']));
+  }
+  let str = "";
+  states_changeArray.forEach(e => {
       str += `<option value="${e}">${e}</option>`;
   })
   selectTag.innerHTML = str;

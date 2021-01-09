f9055fd56b5262aec8c8c9aa9dcc1afd985b69c5
pauld0051 <62159791+pauld0051@users.noreply.github.com>
2021-01-09 11:39:35 +0100

    Update javaScript to allow for state selection after initial country selection is made

--- static/scripts/countries.js
+++ static/scripts/countries.js
@@ -3,24 +3,28 @@
 // Even if the original country was in the countries.json file and then 
 // the user chooses to un-specify, they can then choose "not specified"
 // from the dropdown list.
+$('#state').on('contentChanged', function () {
+  $(this).formSelect();
+});
+
 const selectTag = document.getElementById("state");
-document.getElementById("country").addEventListener("change", function() {
-    const country_change = document.getElementById("country").value;
-    console.log(country_change);
-    console.log(countries);
-    console.log(selectTag);
-    const statesArray = [];
-    const Ind = countries.findIndex(e => {
+document.getElementById("country").addEventListener("change", function () {
+  const country_change = document.getElementById("country").value;
+  console.log(country_change);
+  console.log(countries);
+  console.log(selectTag);
+  const statesArray = [];
+  const Ind = countries.findIndex(e => {
       return e['name'] === country_change;
-})
-if(Ind != -1){
-      countries[Ind]['states'].forEach(e=> statesArray.push(e['name']));
-}
-statesArray.forEach(e => {
-    console.log(e);  
-    const option = document.createElement('option');
-    option.value = "state.name";
-      option.innerText = e;
-      selectTag.appendChild(option);   
   })
+  if (Ind != -1) {
+      countries[Ind]['states'].forEach(e => statesArray.push(e['name']));
+  }
+  let str = "";
+  statesArray.forEach(e => {
+      console.log(e);
+      str += `<option value="${e}">${e}</option>`;
+  })
+  selectTag.innerHTML = str;
+  $("#state").trigger('contentChanged');
 });
